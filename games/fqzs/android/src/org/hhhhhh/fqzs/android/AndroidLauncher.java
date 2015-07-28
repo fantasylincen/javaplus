47c0025b007f6cc7dc51d51f95a16601f4461ca7
12-2 <12-2>
2015-07-28 19:58:45 +0800

    Signed-off-by: 12-2 <12-2>

---
+++ games/fqzs/android/src/org/hhhhhh/fqzs/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.hhhhhh.fqzs.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.hhhhhh.fqzs.Fqzs;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Fqzs(), config);
+	}
+}
---
+++ games/fqzs/desktop/src/org/hhhhhh/fqzs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.hhhhhh.fqzs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.hhhhhh.fqzs.Fqzs;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fqzs(), config);
+	}
+}
