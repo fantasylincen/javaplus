ca01515a53c48dbdd3e84a25e8bf64cbda46e473
lc574907580 <574907580@qq.com>
2015-07-31 18:02:57 +0800

    Signed-off-by: lc574907580 <574907580@qq.com>

--- games/fqzs/core/src/org/hhhhhh/fqzs/result/PlayResultAction.java
+++ games/fqzs/core/src/org/hhhhhh/fqzs/result/PlayResultAction.java
@@ -1,9 +1,13 @@
 package org.hhhhhh.fqzs.result;
 
 import org.hhhhhh.fqzs.core.App;
+import org.hhhhhh.fqzs.core.CoinStatus;
+import org.hhhhhh.fqzs.core.GameStage;
 import org.hhhhhh.fqzs.result.PlayResult.Result;
+import org.javaplus.game.common.assets.Assets;
 import org.javaplus.game.common.log.Log;
 
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
 
 public class PlayResultAction extends RunnableAction {
@@ -18,6 +22,9 @@ public class PlayResultAction extends RunnableAction {
 		public void run() {
 			String name=  App.getTranslator().translate(next.getType());
 			Log.d("play result", next.getId(), next.getType(), name);
+			GameStage stage = App.getApp().getStage();
+			Group group = stage.getGameGroup();
+			group.addActor(new AnimalSpecial());
 		}
 	}
 
