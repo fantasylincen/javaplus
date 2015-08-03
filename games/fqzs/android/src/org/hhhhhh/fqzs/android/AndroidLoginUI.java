f4e2f6f54acdcad303cf60915f489bdf55ffbe52
lc574907580 <574907580@qq.com>
2015-08-03 23:24:38 +0800

    Signed-off-by: lc574907580 <574907580@qq.com>

--- games/fqzs/android/src/org/hhhhhh/fqzs/android/AndroidLoginUI.java
+++ games/fqzs/android/src/org/hhhhhh/fqzs/android/AndroidLoginUI.java
@@ -6,19 +6,26 @@ import org.hhhhhh.fqzs.login.LoginListener;
 import org.hhhhhh.fqzs.login.LoginUI;
 import org.javaplus.game.common.util.Lists;
 
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
+
 import android.app.Activity;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 
 public class AndroidLoginUI implements LoginUI {
 
 	private ArrayList<LoginListener> listeners;
 	private Activity activity;
+	private View username;
+	private View password;
+	private Button logginButton;
 
 	public AndroidLoginUI(Activity activity) {
 		this.activity = activity;
 		listeners = Lists.newArrayList();
+
 	}
+
 	@Override
 	public void addListener(LoginListener listener) {
 		listeners.add(listener);
@@ -26,10 +33,13 @@ public class AndroidLoginUI implements LoginUI {
 
 	@Override
 	public void show() {
-		View v = new ImageView(activity)
-		;
-	activity.addContentView(v, null);
 		activity.setContentView(R.layout.login);
+		if (username == null) {
+			username = activity.findViewById(R.id.username);
+			password = activity.findViewById(R.id.password);
+			logginButton = (Button) activity.findViewById(R.id.loginButton);
+		}
+
 	}
 
 }
